1b668f821e852baeac14832ed312f84a11e2ef02
Aman Shrivastava <ashrivastava3@atlassian.com>
2026-06-10 19:35:15 +0530

    commit 1

--- bamboo-specs/src/main/java/tutorial/PlanSpec.java
+++ bamboo-specs/src/main/java/tutorial/PlanSpec.java
@@ -35,7 +35,7 @@ public class PlanSpec {
                 );
 
         return new Plan(project(), "TESTING", "TESTING")
-                .description("Plan created from Bamboo Java Specs - GitHub repo v1")
+                .description("Plan created from Bamboo Java Specs - GitHub repo v4")
                 .linkedRepositories("GHB")
                 .stages(stage1);
     }
