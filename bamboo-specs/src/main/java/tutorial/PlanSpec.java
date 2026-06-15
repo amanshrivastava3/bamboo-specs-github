eaa81b6d8d29645b15176219ba8e6e7266221461
amanshrivastava3 <59757659+amanshrivastava3@users.noreply.github.com>
2026-06-15 14:33:13 +0530

    Update PlanSpec.java

--- bamboo-specs/src/main/java/tutorial/PlanSpec.java
+++ bamboo-specs/src/main/java/tutorial/PlanSpec.java
@@ -39,7 +39,7 @@ public class PlanSpec {
                 );
 
         return new Plan(project(), "TESTING", "TESTING")
-                .description("Plan created from Bamboo Java Specs - Git repo with polling1111111")
+                .description("Plan created from Bamboo Java Specs - Git repo with pollingtesting")
                 .linkedRepositories("bitbucket-test-repo", "bamboo-specs-github")
                 // Polling trigger every 1 minute on bamboo-specs-github
                 .triggers(new RepositoryPollingTrigger()
